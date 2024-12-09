4d76770ba11446cf5d04fd14ef4fa3615e03cf0b
RAIMBAULT Fabien <git@fabienr.dev>
2024-12-09 21:00:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Script Menu Hamburger
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+});
 
 // Gestion du thème sombre/clair
 const rootElement = document.documentElement;
